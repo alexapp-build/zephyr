83475a65e931ca48a9df9888e07930b6bd923603
alexapp-build <v15308641@gmail.com>
2026-07-03 23:54:26 +0700

    Auto add token by 2098147421

--- ytta.js
+++ ytta.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8971853316:AAFw6dYU4ljICkpCZ5_VGMTcoPT2HUnxEOI"
+    "8971853316:AAFw6dYU4ljICkpCZ5_VGMTcoPT2HUnxEOI",
+    "8885241800:AAE3bGjGPfnyGDzwDKuxUXSud2qgvaLT9eg"
   ]
 }
